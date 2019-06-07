01c3213fa642db6abddb4baa2d9b971ade688cfe
KimberlySwinton <kimberlyswinton@gmail.com>
2019-06-07 13:28:17 -0400

    Fixed Imports, List is showing

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './StarWars.css';
 
 const Character = props => {
-    return (
-        <div className='character-container'>
-        <h1>Name:</h1> 
-        <p> {props.Character.name}</p>
+  return (
+    <div className='character-container'>
+      <h1>Name:</h1>
+      <p> {props.Character.name}</p>
       <h1>Gender:</h1>
       <p> {props.Character.gender}</p>
       <h1>Hair Color:</h1>
       <p> {props.Character.hair_color}</p>
     </div>
-    );
+  );
 };
 
-
 export default Character;
+
